47ff64921b2e3ef82abfd9f66aecabdfbb6154c5
Tbot101 <44803038+Tbot101@users.noreply.github.com>
2021-01-07 10:06:53 +0800

    Blank

--- password.py
+++ password.py
@@ -1,18 +1,10 @@
 def spotify_user_id():
-    return 'stealthforce10'
+    return ''
 
 
 def spotify_token():
-    return 'BQB69wG0o7yYGAG-pCHNS4O5RdTaueClZHqbVxGz-LrvuROgtQmHq5-pAwwEgitp4rce4FFnYH6xanLSFQLr9X4bMA1ieYfG0ypAkQ57CJTsUrB5tL0QUWGeIDc1xHlwSlxamYrprU7ViZdLhf45K2Y7Q9xFOSPDTR-9KsQqAOTM0YwSxxnN'
+    return ''
 
 
 def last_fm_api_key():
-    return 'f101ad43c4a598af92c91bebdf1383b4'
-
-#  Last_fm
-# Here are the details of your new API account.
-#
-# Application name	Spotify Project
-# API key	f101ad43c4a598af92c91bebdf1383b4
-# Shared secret	11e234707ac3166710a5e4ac605cfa42
-# Registered to	Tbot101
+    return ''
